2f9227e61007a40cd0e3b1366a01a5d938b22a93
yuhaifeng <yuhaifeng0515@126.com>
2020-10-17 15:25:00 +0800

    输出hello new3 compare with current

--- src/main/java/com/yhf/test/Test.java
+++ src/main/java/com/yhf/test/Test.java
@@ -7,6 +7,7 @@ public class Test {
         System.out.println("hello new2");
         System.out.println("hello new3");
         System.out.println("hello new3 to main");
+        System.out.println("hello new3 compare with current");
         System.out.println("hello main compare with current");
     }
 }
