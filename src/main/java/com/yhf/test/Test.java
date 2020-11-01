7993c39a52f7af66ed2e6f06b62dbf93982946d6
yuhaifeng0515 <70041578+yuhaifeng0515@users.noreply.github.com>
2020-11-01 17:53:41 +0800

    add 6

--- src/main/java/com/yhf/test/Test.java
+++ src/main/java/com/yhf/test/Test.java
@@ -14,6 +14,8 @@ public class Test {
         System.out.println("----2------");
         System.out.println("----3------");
         
+        System.out.println("----6------");
+        
         System.out.println("----4------");
         System.out.println("----5------");
     }
